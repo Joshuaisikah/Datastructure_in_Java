f8f6c6c2022b75a02dea44b03407cfb0332cbb31
Joshuaisikah <joshuaiska@gmail.com>
2024-01-06 23:47:19 +0300

    java practise codes

--- untitled4/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-   
-    public static void main(String[] args) {
-        Main mis=new Main();
-        int arr[]={1,2,3,4,5,7,8,9,10,11};
-        int missing= mis.find1(arr);
-        System.out.println(missing);
-    }
-}
--- untitled5/src/issah.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class issah {
-    private JPanel panel1;
-
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
-    }
-}
